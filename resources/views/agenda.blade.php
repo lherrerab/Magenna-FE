d43cd33a69c60d561d9ccf89923b01e880bb9646
Leonardo Herrera <herreraleonardo@hotmail.com>
2015-11-01 18:16:43 -0500

    Update agenda.blade.php

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -1,10 +1,10 @@
 @extends('layout.dashboard')
 
 @section('sidebar')
-            			<li><a href="/magenna">Dashboard <span class="sr-only">(current)</span></a></li>
-            			<li><a href="/magenna/contacts" ng-click="activateContacts()">Contacts</a></li>
-            			<li class="active"><a href="#">Agenda</a></li>
-            			<li><a href="/magenna/notes">Notes</a></li>
+            <li><a href="/magenna">Dashboard <span class="sr-only">(current)</span></a></li>
+            <li><a href="/magenna/contacts" ng-click="activateContacts()">Contacts</a></li>
+            <li class="active"><a href="#">Agenda</a></li>
+            <li><a href="/magenna/notes">Notes</a></li>
 @stop
 
 @section('content')
