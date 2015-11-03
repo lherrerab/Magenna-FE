d58567a6c91169dc08356cff29e55a22c84701a8
Leonardo Herrera <herreraleonardo@hotmail.com>
2015-11-02 22:31:09 -0500

    Update notes.blade.php

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -1,10 +1,10 @@
 @extends('layout.dashboard')
 
 @section('sidebar')
-            			<li><a href="/magenna">Dashboard <span class="sr-only">(current)</span></a></li>
-            			<li><a href="/magenna/contacts" ng-click="activateContacts()">Contacts</a></li>
-            			<li><a href="/magenna/agenda">Agenda</a></li>
-            			<li class="active"><a href="#">Notes</a></li>
+            <li><a href="/magenna">Dashboard <span class="sr-only">(current)</span></a></li>
+            <li><a href="/magenna/contacts" ng-click="activateContacts()">Contacts</a></li>
+            <li><a href="/magenna/agenda">Agenda</a></li>
+            <li class="active"><a href="#">Notes</a></li>
 @stop
 
 @section('content')     
